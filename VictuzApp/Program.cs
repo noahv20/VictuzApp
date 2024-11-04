954b489935989a709e1d34c6a5ba3d1538fce8aa
Noah Vranken <2303367vranken@zuyd.nl>
2024-11-04 13:53:51 +0100

    foutje

--- VictuzApp/Program.cs
+++ VictuzApp/Program.cs
@@ -38,7 +38,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Eventds}/{action=Index}/{id?}");
+    pattern: "{controller=Events}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 using (var scope = app.Services.CreateScope())
