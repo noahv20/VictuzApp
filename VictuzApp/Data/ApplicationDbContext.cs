7b09844f0844062be0d15bc47265f00f1a04a509
Noah Vranken <2303367vranken@zuyd.nl>
2024-11-07 14:27:52 +0100

    View Registrations(werkt nog niet)

--- VictuzApp/Data/ApplicationDbContext.cs
+++ VictuzApp/Data/ApplicationDbContext.cs
@@ -7,7 +7,6 @@ namespace VictuzApp.Data
     public class ApplicationDbContext : IdentityDbContext
     {
         public DbSet<Event> Events { get; set; }
-
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
