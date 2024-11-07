3884afa9b7f64a5f84e24585f3e05750737b3d6e
Noah Vranken <2303367vranken@zuyd.nl>
2024-11-07 11:13:06 +0100

    Rollen aanpassen voor administrator af

--- VictuzApp/Controllers/HomeController.cs
+++ VictuzApp/Controllers/HomeController.cs
@@ -1,16 +1,83 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using System;
 using System.Diagnostics;
+using VictuzApp.Data;
 using VictuzApp.Models;
+using VictuzApp.ViewModels;
 
 namespace VictuzApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;    
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext context,ILogger<HomeController> logger, UserManager<IdentityUser> userManager,RoleManager<IdentityRole> roleManager)
         {
+            _context = context;
             _logger = logger;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+        [Authorize(Roles ="Admin")]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return View(users);
+        }
+        [HttpGet]
+        [Authorize(Roles ="Admin")]
+        public async Task<IActionResult> ChangeUserRole(string userId)
+        {
+            if (userId == null)
+            {
+                return NotFound();
+            }
+            var user = await _userManager.FindByIdAsync(userId);
+            var roles = await _context.Roles.ToListAsync();
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            var selectList = new List<SelectListItem>();
+            foreach (var role in roles) 
+            {
+                selectList.Add(new SelectListItem(value: role.Name, text: role.Name));
+            };
+
+
+
+            RoleUser roleUser = new RoleUser()
+            {
+                User = user,
+                UserRoles = userRoles,
+                SelectRoles = selectList
+            };
+            return View(roleUser);
+        }
+        [HttpPost]
+        [Authorize(Roles ="Admin")]
+        public async Task<IActionResult> ChangeRole(string userId, string role)
+        {
+            if (userId == null || role == null)
+            {
+                return NotFound();
+            }
+            
+            var user = await _userManager.FindByIdAsync(userId);
+            var currentUserRoles = await _userManager.GetRolesAsync(user);
+
+            string userRole = currentUserRoles[0];
+            
+            await _userManager.RemoveFromRoleAsync(user, userRole);
+            await _userManager.AddToRoleAsync(user, role);
+
+            return RedirectToAction(nameof(ChangeUserRole), new { userId });
         }
 
         public IActionResult Index()
---
+++ VictuzApp/ViewModels/RoleUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace VictuzApp.ViewModels
+{
+    public class RoleUser
+    {
+        public IdentityUser User { get; set; }
+        public IEnumerable<SelectListItem> SelectRoles { get; set; }
+        public IEnumerable<string> UserRoles {  get; set; }
+    }
+}
